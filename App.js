bb32b56971bbcd84cf976c8c742bfea303a01b76
Zicronci <mesarodriguezgustavoangel55@gmail.com>
2021-02-25 12:42:46 -0600

    configuracion de sass

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import estilosSass from './styles/Start.scss';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Study IQ xd</Text>
       <StatusBar style="auto" />
+      <Text style={estilosSass.xd}> si funciona los estilos de sass</Text>
     </View>
   );
 }
