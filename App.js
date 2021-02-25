00faa64bdf1274ad1619d0469fbf1b3e34107e2d
Zicronci <mesarodriguezgustavoangel55@gmail.com>
2021-02-25 10:43:34 -0600

    solo le puse xd

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Study IQ</Text>
+      <Text>Study IQ xd</Text>
       <StatusBar style="auto" />
     </View>
   );
